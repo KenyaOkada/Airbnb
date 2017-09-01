449b60f56f7522002aacd8ee33a93e1e90de89c2
岡田兼弥 <okada@okadakenwatarunoMacBook-Air.local>
2017-09-01 16:32:12 +0900

    edit_update

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -22,10 +22,24 @@ class HousesController < ApplicationController
    end
  end
 
+ def edit
+   @house = House.find(params[:id])
+ end
+
+ def update
+   @house = House.find(params[:id])
+   if @house.update(house_params)
+     redirect_to houses_path
+   else
+     render 'edit'
+ end
+
 
  private
    def house_params
      params.require(:house).permit(:house_name, :house_description, :price, :adress)
    end
+
+
 
 end
